0c63071fc3a22a5d2e86943bb9ffbf33b4ba893e
anhn <hoaianh@gmail.com>
2024-05-24 06:50:21 +0700

    rename

--- src/service/impl/wireguard.go
+++ src/service/impl/wireguard.go
@@ -10,36 +10,36 @@ import (
 	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 )
 
-type WGManager struct {
-	WGInterface string
-	client      *wgctrl.Client
+type wgDeviceManager struct {
+	device string
+	client *wgctrl.Client
 }
 
-func NewWGManager(wgInterface string) (*WGManager, error) {
+func NewWGManager(wgInterface string) (*wgDeviceManager, error) {
 	wgClient, err := wgctrl.New()
 	if err != nil {
 		return nil, err
 	}
-	return &WGManager{
-		WGInterface: wgInterface,
-		client:      wgClient,
+	return &wgDeviceManager{
+		device: wgInterface,
+		client: wgClient,
 	}, nil
 }
 
-func (wgm *WGManager) GetPublicKey() (iface.PublicKey, error) {
-	wgDevice, err := wgm.client.Device(wgm.WGInterface)
+func (wgm *wgDeviceManager) GetPublicKey() (iface.PublicKey, error) {
+	wgDevice, err := wgm.client.Device(wgm.device)
 	if wgDevice == nil || err != nil {
 		return iface.PublicKey{}, err
 	}
 	return iface.PublicKey{Key: wgDevice.PublicKey}, nil
 }
 
-func (wgm *WGManager) GeneratePrivateKey() (iface.PrivateKey, error) {
+func (wgm *wgDeviceManager) GeneratePrivateKey() (iface.PrivateKey, error) {
 	privateKey, err := wgtypes.GeneratePrivateKey()
 	return iface.PrivateKey{Key: privateKey}, err
 }
 
-func (wgm *WGManager) ConfigureWG(ctx context.Context, peers []iface.Peer) error {
+func (wgm *wgDeviceManager) Configure(ctx context.Context, peers []iface.Peer) error {
 	wgPeers := []wgtypes.PeerConfig{}
 
 	for _, peer := range peers {
@@ -55,14 +55,14 @@ func (wgm *WGManager) ConfigureWG(ctx context.Context, peers []iface.Peer) error
 		ReplacePeers: true,
 		Peers:        wgPeers,
 	}
-	err := wgm.client.ConfigureDevice(wgm.WGInterface, cfg)
+	err := wgm.client.ConfigureDevice(wgm.device, cfg)
 	if err != nil {
 		return fmt.Errorf("error configuring WireGuard device: %w", err)
 	}
 	return nil
 }
 
-func (wgm *WGManager) AddPeers(ctx context.Context, peers []iface.Peer) error {
+func (wgm *wgDeviceManager) AddPeers(ctx context.Context, peers []iface.Peer) error {
 	wgPeers := []wgtypes.PeerConfig{}
 
 	for _, peer := range peers {
@@ -79,14 +79,14 @@ func (wgm *WGManager) AddPeers(ctx context.Context, peers []iface.Peer) error {
 		ReplacePeers: false,
 		Peers:        wgPeers,
 	}
-	err := wgm.client.ConfigureDevice(wgm.WGInterface, cfg)
+	err := wgm.client.ConfigureDevice(wgm.device, cfg)
 	if err != nil {
 		return fmt.Errorf("error adding peer to WireGuard: %w", err)
 	}
 	return nil
 }
 
-func (wgm *WGManager) RemovePeers(ctx context.Context, publicKeys []iface.PublicKey) error {
+func (wgm *wgDeviceManager) RemovePeers(ctx context.Context, publicKeys []iface.PublicKey) error {
 	wgPeers := []wgtypes.PeerConfig{}
 
 	for _, publicKey := range publicKeys {
@@ -100,7 +100,7 @@ func (wgm *WGManager) RemovePeers(ctx context.Context, publicKeys []iface.Public
 		ReplacePeers: false,
 		Peers:        wgPeers,
 	}
-	err := wgm.client.ConfigureDevice(wgm.WGInterface, cfg)
+	err := wgm.client.ConfigureDevice(wgm.device, cfg)
 	if err != nil {
 		return fmt.Errorf("error removing peers from WireGuard: %w", err)
 	}
@@ -111,8 +111,8 @@ func (wgm *WGManager) RemovePeers(ctx context.Context, publicKeys []iface.Public
 // performed with the client in the last 3 minutes. If a WireGuard client
 // did not perform a handshake in the last 3 minutes, all packets will be
 // dropped by WireGuard until the client performs a new handshake.
-func (wgm *WGManager) GetConnections(ctx context.Context) ([]wgtypes.Peer, error) {
-	wgDevice, err := wgm.client.Device(wgm.WGInterface)
+func (wgm *wgDeviceManager) GetConnections(ctx context.Context) ([]wgtypes.Peer, error) {
+	wgDevice, err := wgm.client.Device(wgm.device)
 	if wgDevice == nil || err != nil {
 		return nil, err
 	}
@@ -132,25 +132,25 @@ func (wgm *WGManager) GetConnections(ctx context.Context) ([]wgtypes.Peer, error
 	return peers, nil
 }
 
-func (wgm *WGManager) Close() error {
+func (wgm *wgDeviceManager) Close() error {
 	return wgm.client.Close()
 }
 
 // Initialize initializes the Wireguard service
-func (s *WGManager) Initialize(ctx context.Context) error {
+func (s *wgDeviceManager) Initialize(ctx context.Context) error {
 	return nil
 }
 
 // Start starts the Wireguard service
-func (s *WGManager) Start(ctx context.Context) error {
+func (s *wgDeviceManager) Start(ctx context.Context) error {
 	return nil
 }
 
 // Stop stops the Wireguard service
-func (s *WGManager) Stop(ctx context.Context) error {
+func (s *wgDeviceManager) Stop(ctx context.Context) error {
 	return nil
 }
 
-func (s *WGManager) HealthCheck(ctx context.Context) error {
+func (s *wgDeviceManager) HealthCheck(ctx context.Context) error {
 	return nil
 }
